a206b600035f38beead2d01104c35c8bda51250f
abdolahbd <63928697+abdolahbd@users.noreply.github.com>
2022-04-09 12:52:08 +0000

    Update khzm1.py

--- khzm1.py
+++ khzm1.py
@@ -11,8 +11,16 @@ def home():
     time.sleep(2)
     print(1)
     s = requests.Session()
-    r = s.get('https://abdollah-prjt.herokuapp.com/',timeout=1)
-    r = s.get('http://cfd8-105-71-19-104.ngrok.io',timeout=1)
+    
+    try:
+        r = s.get('https://abdollah-prjt.herokuapp.com/',timeout=1)
+    except:
+        pass
+    try:
+        r = s.get('http://cfd8-105-71-19-104.ngrok.io',timeout=1)
+    except:
+        pass
+    
     return "I'm alive"
 
 
