99d97421e73ad617d256ef1a93b700fa6ab71336
abdolahbd <63928697+abdolahbd@users.noreply.github.com>
2022-04-09 11:30:57 +0000

    Update khzm1.py

--- khzm1.py
+++ khzm1.py
@@ -19,9 +19,15 @@ class index:
         # Making a get request
         # create a session object
         s = requests.Session()
-
-
-
+        
+        try:
+            r = s.get('https://abdollah-prjt.herokuapp.com/',timeout=1)
+        except:
+            pass
+        try:
+            r = s.get('http://127.0.0.1:1234/',timeout=1)
+        except:
+            pass
       
 
         # check if cookie is still set
