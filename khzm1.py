23a38326cacd5386d71dc2f60d30b7a0e547c564
abdolahbd <63928697+abdolahbd@users.noreply.github.com>
2022-04-09 18:32:04 +0000

    Update khzm1.py

--- khzm1.py
+++ khzm1.py
@@ -15,8 +15,11 @@ def home():
     print(1)
     s = requests.Session()
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0'}
-    r = s.get('https://abdollah-prjt.herokuapp.com',headers=headers)
-    r = s.get('https://khzm1.herokuapp.com',headers=headers)
+    try:
+        r = s.get('https://abdollah-prjt.herokuapp.com',headers=headers)
+        r = s.get('https://khzm1.herokuapp.com',headers=headers)
+    except:
+        pass
 
     return "I'm alive"
 
