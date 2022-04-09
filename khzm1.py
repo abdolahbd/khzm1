6a433ad309ddb8cf41cfae7669f8d07812b8497a
abdolahbd <63928697+abdolahbd@users.noreply.github.com>
2022-04-09 18:40:29 +0000

    Update khzm1.py

--- khzm1.py
+++ khzm1.py
@@ -1,7 +1,3 @@
-from datetime import datetime
-
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 import time
 import requests
 from flask import Flask
@@ -11,15 +7,13 @@ app = Flask('')
 
 @app.route('/')
 def home():
+    import socket
     time.sleep(5)
     print(1)
     s = requests.Session()
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0'}
-    try:
-        r = s.get('https://abdollah-prjt.herokuapp.com',headers=headers)
-        r = s.get('https://khzm1.herokuapp.com',headers=headers)
-    except:
-        pass
+    r = s.get('https://abdollah-prjt.herokuapp.com',headers=headers)
+    r = s.get('http://localhost:5000',headers=headers)
 
     return "I'm alive"
 
@@ -29,4 +23,4 @@ def run():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5000)
