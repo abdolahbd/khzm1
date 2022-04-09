a5225e8a75c0de4e960e0cb7504e92857f0dfe78
abdolahbd <63928697+abdolahbd@users.noreply.github.com>
2022-04-09 18:48:46 +0000

    Update khzm1.py

--- khzm1.py
+++ khzm1.py
@@ -8,14 +8,14 @@ app = Flask('')
 @app.route('/')
 def home():
     import socket
-    time.sleep(5)
+    time.sleep(20)
     print(1)
     s = requests.Session()
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0'}
     
     try:
         r = s.get('https://abdollah-prjt.herokuapp.com',headers=headers)
-        r = s.get('http://localhost:5000',headers=headers)
+        r = s.get('https://khzm2.herokuapp.com/',headers=headers)
     except:
         pass
 
@@ -27,4 +27,4 @@ def run():
 
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run()
